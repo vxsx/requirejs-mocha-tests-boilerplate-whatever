288751e7e2861d4741a99858a1934489ca913b71
Vadim Sikora <vadim.sikora@gmail.com>
2013-08-10 20:28:59 +0200

    Added more dummy tests
    Example with sinon

--- test/module.js
+++ test/module.js
@@ -2,16 +2,35 @@
 /* jshint -W030: true */
 define([
     'chai',
-    'js/module'
-], function (chai, Module) {
+    'sinon',
+    'sinonchai',
+    'js/module',
+    'jquery'
+], function (chai, sinon, sinonChai, Module, $) {
     'use strict';
 
     var expect = chai.expect;
+    chai.use(sinonChai);
 
     describe('dummy module', function () {
         it('should be an object', function () {
             expect(Module).to.not.be.an('undefined');
             expect(Module).to.be.an('object');
+        });
+
+        it('should have some members', function () {
+            expect(Module.a).to.eq('b');
+            expect(Module.$).to.eql($);
+        });
+
+        it('sinon works too', function () {
+            var spy = sinon.spy(Module, '$');
+
+            Module.$('body');
+
+            expect(spy).to.have.been.calledOnce;
+
+            spy.restore();
         });
     });
 });
